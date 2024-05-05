bf64cf34a32b71a66b5708e8cb39dfddcc616a83
ElizetSolanoEspinoza <167382251+ElizetSolanoEspinoza@users.noreply.github.com>
2024-05-04 19:57:32 -0600

    Add files via upload

---
+++ encabezado.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<center><img width="750" height="220" src="nueva_imagen.jpg"/>
+<h3 id="centrado">ENCUESTA A ING EN ISIC
+CAMPUS MONTAÑA
+</h3>
+</body>
+</html>
---
+++ pie.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title></title>
+<link rel="stylesheet" href="css/estilo.css">
+</head>
+<body>
+<h4 id="centrado">Todos los derechos reservados
+Elizet Solano Espinoza@2002</h4>
+<center><img width="120" height="120" src="http://www.kimicamerica.com/b8.gif"/>
+</body>
+</html>
